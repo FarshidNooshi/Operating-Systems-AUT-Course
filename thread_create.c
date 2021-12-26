025d0a11fd6e489ec7a399632636ffec1a84e129
FarshidNooshi <farshidnooshi726@aut.ac.ir>
2021-12-26 12:00:43 +0330

    thread_create implemented

---
+++ thread_create.c
@@ -0,0 +1,31 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+#define PAGESIZE 4096
+
+int thread_create(void (*fn)(void *), void *arg)
+{
+    // allocating 2 * pageSize for fptr in heap
+    void *fptr = malloc(2 * (PAGESIZE));
+    void *stack;
+    if (fptr == 0)
+        return -1;
+    int mod = (uint)fptr % PAGESIZE;
+    // the following if-else is for assigning page-aligned space to stack
+    if (mod == 0)
+        stack = fptr;
+    else
+        stack = fptr + (PAGESIZE - mod);
+    int thread_id = clone((void *)stack);
+    // clone failed
+    if (thread_id < 0)
+        printf(1, "clone failed\n");
+    else if (thread_id == 0)
+    { // child
+        (fn)(arg);
+        free(stack);
+        exit();
+    }
+    return thread_id;
+}
